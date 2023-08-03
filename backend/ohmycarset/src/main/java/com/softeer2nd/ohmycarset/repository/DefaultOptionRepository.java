0ef26c33fca83d2a1b96b9e4e73bc5f0a0ef9a17
Jaehong Kim <kjhonggg95@gmail.com>
2023-08-03 16:15:49 +0900

    feat: DefaultOptionRepository 인터페이스 생성
    DefaultOptionService 에서 구현함.

---
+++ backend/ohmycarset/src/main/java/com/softeer2nd/ohmycarset/repository/DefaultOptionRepository.java
@@ -0,0 +1,12 @@
+package com.softeer2nd.ohmycarset.repository;
+
+import com.softeer2nd.ohmycarset.domain.DefaultOption;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DefaultOptionRepository {
+    public List<DefaultOption> findAll();
+    public Optional<DefaultOption> findById(Long id);
+    public List<DefaultOption> findByTrimIdAndDefCategoryId(Long trimId, Long defCategoryId);
+}
