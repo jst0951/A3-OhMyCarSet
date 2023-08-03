0fe49742e63c740c67877edd11f3d03b9ab26001
yen001004 <yenn001004@gmail.com>
2023-08-04 01:34:37 +0900

    feat: 트림 카드 컴포넌트 구현

--- Frontend/src/components/main/LandingMain/LandingMain.tsx
+++ Frontend/src/components/main/LandingMain/LandingMain.tsx
@@ -1,16 +1,30 @@
+import Icon from '@/components/common/Icon';
+import TrimCardList from '../TrimCard/TrimCard';
 import * as Style from './LandingMain.style';
 
 function LandingMain() {
   return (
     <>
-      <Style.LandingMainBackground>
-        <Style.LandingMainContainer>
-          <Style.LandingMainTextContainer>
-            <Style.LandingSubText>내 차 만들기</Style.LandingSubText>
-            <Style.LandingMainText>PALISADE</Style.LandingMainText>
-          </Style.LandingMainTextContainer>
-        </Style.LandingMainContainer>
-      </Style.LandingMainBackground>
+      <Style.LandingMainContainer>
+        <Style.LandingMainBackground>
+          <Style.LandingMainContent>
+            <Style.LandingMainTextContainer>
+              <Style.LandingSubText>내 차 만들기</Style.LandingSubText>
+              <Style.LandingMainText>PALISADE</Style.LandingMainText>
+            </Style.LandingMainTextContainer>
+          </Style.LandingMainContent>
+          <Style.LandingDimmed>
+            <TrimCardList />
+          </Style.LandingDimmed>
+        </Style.LandingMainBackground>
+        <Style.ShowMoreContainer>
+          <Style.ShowMoreText>자세한 설명과 비교를 원한다면</Style.ShowMoreText>
+          <Style.IconWrapper>
+            <Icon icon="ShowMoreIcon" size={26} />
+            <Icon icon="ShowMoreIcon" size={26} />
+          </Style.IconWrapper>
+        </Style.ShowMoreContainer>
+      </Style.LandingMainContainer>
     </>
   );
 }
---
+++ Frontend/src/components/main/TrimDescription/TrimDescription.tsx
@@ -0,0 +1,41 @@
+import * as Style from './TrimDescription.style';
+import type { TrimCardProps } from '../TrimCard/TrimCard';
+
+type TrimInfoProps = {
+  subTitle: string;
+  subImage: boolean;
+  descriptionData: Array<{
+    main: string;
+    sub: string;
+  }>;
+};
+
+interface TrimInformationProps {
+  trimInfo: TrimInfoProps;
+}
+
+function TrimInformation({ trimInfo }: TrimInformationProps) {
+  const { descriptionData } = trimInfo;
+  console.log(descriptionData);
+  return <></>;
+}
+
+export default function TrimDescription({ trimData }: TrimCardProps) {
+  const subTitle = trimData.descriptionCard?.subTitle || '';
+  const { name, descriptionCard } = trimData;
+  return (
+    <>
+      <Style.TrimDescriptionCard>
+        <Style.TrimDescriptionBorder>
+          <Style.TitleContainer>
+            <Style.TrimSubTitle>{subTitle}</Style.TrimSubTitle>
+            <Style.TrimTitle>{name}</Style.TrimTitle>
+          </Style.TitleContainer>
+          <Style.TrimInformationContainer>
+            {descriptionCard && <TrimInformation trimInfo={descriptionCard} />}
+          </Style.TrimInformationContainer>
+        </Style.TrimDescriptionBorder>
+      </Style.TrimDescriptionCard>
+    </>
+  );
+}
