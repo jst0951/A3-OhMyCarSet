209e4da6fe2f3876378ab2b6397071440d23be48
jst0951 <jst0951@gmail.com>
2023-08-03 14:37:39 +0900

    feat: CORS 허용 범위 추가

---
+++ backend/ohmycarset/src/main/java/com/softeer2nd/ohmycarset/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.softeer2nd.ohmycarset.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로에 대해
+                .allowedOrigins("http://localhost:5173", "https://localhost:5173", "http://ohmycarset.com:80", "https://ohmycarset.com:443") // 허용할 도메인 목록
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메소드 목록
+                .allowedHeaders("*") // 허용할 헤더 목록
+                .allowCredentials(true); // 쿠키를 주고받을 수 있도록 허용 여부
+    }
+}
