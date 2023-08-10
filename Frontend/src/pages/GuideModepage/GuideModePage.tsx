1cc56bff058924fe74bcd0293f15ed3bff774632
hb9901 <hb9901@gmail.com>
2023-08-10 10:58:24 +0900

    feat: guidemode 페이지 추가

---
+++ Frontend/src/pages/GuideModepage/GuideModePage.tsx
@@ -0,0 +1,12 @@
+import Layout from '@/components/layout/Layout';
+import * as Style from './GuideModePage.style';
+
+export default function SelfModePage() {
+  return (
+    <>
+      <Layout>
+        <Style.GuideModeContainer></Style.GuideModeContainer>
+      </Layout>
+    </>
+  );
+}
