b516c4189fcacccba2f712bbf47bf7eab457fb33
Jaehong Kim <kjhonggg95@gmail.com>
2023-08-10 19:13:21 +0900

    test: 테스트 데이터 스키마 생성 및 데이터 삽입 쿼리 작성

--- backend/ohmycarset/src/test/resources/schema.sql
+++ backend/ohmycarset/src/test/resources/schema.sql
@@ -5,8 +5,63 @@ CREATE TABLE exterior_color (
     img_src VARCHAR(200)
 );
 
+CREATE TABLE trim (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(40),
+    `description` VARCHAR(40),
+    default_price INT,
+    rep_color_id BIGINT,
+    isBest BOOLEAN,
+    FOREIGN KEY (rep_color_id) REFERENCES exterior_color (id)
+);
+
+CREATE TABLE trim_exterior_color_map (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    e_color_id BIGINT,
+    trim_id BIGINT,
+    FOREIGN KEY (e_color_id) REFERENCES exterior_color(id),
+    FOREIGN KEY (trim_id) REFERENCES trim (id)
+);
+
 CREATE TABLE interior_color (
     id BIGINT PRIMARY KEY,
     `name` VARCHAR(40),
     img_src VARCHAR(200)
+);
+
+CREATE TABLE trim_interior_color_map (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    i_color_id BIGINT,
+    trim_id BIGINT,
+    FOREIGN KEY (i_color_id) REFERENCES interior_color(id),
+    FOREIGN KEY (trim_id) REFERENCES trim (id)
+);
+
+CREATE TABLE default_category (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    `name` varchar(40)
+);
+
+CREATE TABLE default_option (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    `name` varchar(255),
+    img_src varchar(255),
+    def_category_id BIGINT,
+    FOREIGN KEY (def_category_id) REFERENCES default_category (id)
+);
+
+CREATE TABLE trim_default_option_map (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    def_option_id BIGINT,
+    trim_id BIGINT,
+    FOREIGN KEY (def_option_id) REFERENCES default_option(id),
+    FOREIGN KEY (trim_id) REFERENCES trim (id)
+);
+
+CREATE TABLE core_option (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+   `name` varchar(40),
+    trim_id BIGINT,
+    img_src varchar(200),
+    FOREIGN KEY (trim_id) REFERENCES trim (id)
 );
