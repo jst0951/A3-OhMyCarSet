664f680b00ceed4e83f9f699159ab9bd2c79935c
Jaehong Kim <kjhonggg95@gmail.com>
2023-08-10 19:13:58 +0900

    test: InteriorColorRepository 테스트 작성

--- backend/ohmycarset/src/test/java/com/softeer2nd/ohmycarset/repository/InteriorColorRepositoryTest.java
+++ backend/ohmycarset/src/test/java/com/softeer2nd/ohmycarset/repository/InteriorColorRepositoryTest.java
@@ -9,14 +9,21 @@ import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 
 @JdbcTest
 @ActiveProfiles("test")
-@Sql(scripts = {"classpath:/insert/insert_interior_color.sql"})
+@Sql(scripts = {
+        "classpath:/insert/insert_exterior_color.sql",
+        "classpath:/insert/insert_trim.sql",
+        "classpath:/insert/insert_interior_color.sql",
+        "classpath:/insert/insert_trim_interior_color_map.sql"
+})
 class InteriorColorRepositoryTest {
 
     private final InteriorColorRepository interiorColorRepository;
@@ -31,5 +38,29 @@ class InteriorColorRepositoryTest {
     void findAll() {
         List<InteriorColor> interiorColors = interiorColorRepository.findAll();
         assertThat(interiorColors).hasSize(10);
+    }
+
+    @Test
+    @DisplayName("내장색상 ID 에 해당하는 정보를 가져옵니다.")
+    void findById() {
+        Long trimId = 1L;
+        Optional<InteriorColor> interiorColor = interiorColorRepository.findById(trimId);
+        assertThat(interiorColor).isPresent();
+        assertThat(interiorColor.get().getId()).isEqualTo(trimId);
+        assertThat(interiorColor.get().getName()).isEqualTo("인조가죽(블랙)");
+        assertThat(interiorColor.get().getImgSrc()).isEqualTo("interior_color/1.png");
+    }
+
+    @Test
+    @DisplayName("트림 별 내장색상 정보를 가져옵니다.")
+    void findAllByTrimId() {
+        Long trimId = 2L;
+
+        List<InteriorColor> interiorColors = interiorColorRepository.findAllByTrimId(trimId);
+        assertThat(interiorColors).hasSize(2);
+        assertThat(interiorColors.get(0).getName()).isEqualTo("퀼팅 천연(블랙)");
+        assertThat(interiorColors.get(0).getImgSrc()).isEqualTo("interior_color/2.png");
+        assertThat(interiorColors.get(1).getName()).isEqualTo("쿨그레이");
+        assertThat(interiorColors.get(1).getImgSrc()).isEqualTo("interior_color/3.png");
     }
 }
