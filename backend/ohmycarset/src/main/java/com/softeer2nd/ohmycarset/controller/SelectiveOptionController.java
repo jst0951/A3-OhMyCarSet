edf56cb8dbd3b0ce0acb2d4e2e0023daf5921a7e
Jaehong Kim <kjhonggg95@gmail.com>
2023-08-13 18:56:18 +0900

    refactor: 각 엔드포인트 접근을 @PathVariable 로 하도록 변경

--- backend/ohmycarset/src/main/java/com/softeer2nd/ohmycarset/controller/SelectiveOptionController.java
+++ backend/ohmycarset/src/main/java/com/softeer2nd/ohmycarset/controller/SelectiveOptionController.java
@@ -3,6 +3,7 @@ package com.softeer2nd.ohmycarset.controller;
 import com.softeer2nd.ohmycarset.dto.selectiveOptionDto.*;
 import com.softeer2nd.ohmycarset.service.SelectiveOptionService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,53 +16,13 @@ public class SelectiveOptionController {
         this.selectiveOptionService = selectiveOptionService;
     }
 
-    @GetMapping(value = "/selective_option/powertrain")
-    public List<SelectiveOptionDefaultDto> getAllPowerTrain() {
-        return selectiveOptionService.getAllPowerTrainOption();
+    @GetMapping(value = "/selective_option/{optionName}")
+    public List<RequiredOptionDto> getOptionByName(@PathVariable String optionName) {
+        return selectiveOptionService.getOptionByName(optionName);
     }
 
-    @GetMapping(value = "/selective_option/wd")
-    public List<SelectiveOptionDefaultDto> getAllWD() {
-        return selectiveOptionService.getAllWDOption();
-    }
-
-    @GetMapping(value = "/selective_option/body")
-    public List<SelectiveOptionDefaultDto> getAllBody() {
-        return selectiveOptionService.getAllBodyOption();
-    }
-
-    @GetMapping(value = "/selective_option/exterior_color")
-    public List<SelectiveOptionDefaultDto> getAllExteriorColor() {
-        return selectiveOptionService.getAllExteriorColorOption();
-    }
-
-    @GetMapping(value = "/selective_option/interior_color")
-    public List<SelectiveOptionDefaultDto> getAllInteriorColor() {
-        return selectiveOptionService.getAllInteriorColorOption();
-    }
-
-    @GetMapping(value = "/selective_option/wheel")
-    public List<SelectiveOptionDefaultDto> getAllWheel() {
-        return selectiveOptionService.getAllWheelOption();
-    }
-
-    @GetMapping(value = "/selective_option/system")
-    public List<SelectiveOptionAdditionalDto> getAllSystem() {
-        return selectiveOptionService.getAllSystemOption();
-    }
-
-    @GetMapping(value = "/selective_option/temperature")
-    public List<SelectiveOptionAdditionalDto> getAllTemperature() {
-        return selectiveOptionService.getAllTemperatureOption();
-    }
-
-    @GetMapping(value = "/selective_option/external_device")
-    public List<SelectiveOptionAdditionalDto> getAllExternalDevice() {
-        return selectiveOptionService.getAllExternalDeviceOption();
-    }
-
-    @GetMapping(value = "/selective_option/internal_device")
-    public List<SelectiveOptionAdditionalDto> getAllInternalDevice() {
-        return selectiveOptionService.getAllInternalDeviceOption();
+    @GetMapping(value = "/selective_option/{packageName}")
+    public List<OptionPackageDto> getPackageByName(@PathVariable String packageName) {
+        return selectiveOptionService.getPackageByName(packageName);
     }
 }
