7efd4f976aef840f2ef82ba669c8bb581aa03a8e
yen001004 <yenn001004@gmail.com>
2023-08-14 22:07:25 +0900

    feat:CurrentPackageProvider 구현

---
+++ Frontend/src/contexts/CurrentPackageProvider.tsx
@@ -0,0 +1,66 @@
+import { createContext, useContext, useReducer } from 'react';
+
+interface CurrentPackageStateT {
+  filter: number;
+  package: number;
+  option: number;
+}
+
+type CurrentPackageActionT = {
+  type: 'UPDATE_FILTER' | 'UPDATE_PACKAGE' | 'UPDATE_OPTION';
+  payload: number;
+};
+
+const initialState: CurrentPackageStateT = {
+  filter: 1,
+  package: 1,
+  option: 1,
+};
+
+type CurrentPackageDispatchT = (action: CurrentPackageActionT) => void;
+
+const currentPackageReducer = (state: CurrentPackageStateT, action: CurrentPackageActionT): CurrentPackageStateT => {
+  switch (action.type) {
+    case 'UPDATE_FILTER':
+      return {
+        ...state,
+        filter: action.payload,
+      };
+    case 'UPDATE_PACKAGE':
+      return {
+        ...state,
+        package: action.payload,
+      };
+    case 'UPDATE_OPTION':
+      return {
+        ...state,
+        option: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+const CurrentPackageStateContext = createContext<CurrentPackageStateT | undefined>(undefined);
+const CurrentPackageDispatchContext = createContext<CurrentPackageDispatchT | undefined>(undefined);
+
+export const useCurrentPackageState = () => {
+  const state = useContext(CurrentPackageStateContext);
+  if (!state) throw new Error('Cannot find CurrentPackageContext');
+  return state;
+};
+
+export const useCurrentPackageDispatch = () => {
+  const dispatch = useContext(CurrentPackageDispatchContext);
+  if (!dispatch) throw new Error('Cannot find CurrentPackageContext');
+  return dispatch;
+};
+
+export const CurrentPackageProvider = ({ children }: { children: React.ReactNode }) => {
+  const [state, dispatch] = useReducer(currentPackageReducer, initialState);
+
+  return (
+    <CurrentPackageStateContext.Provider value={state}>
+      <CurrentPackageDispatchContext.Provider value={dispatch}>{children}</CurrentPackageDispatchContext.Provider>
+    </CurrentPackageStateContext.Provider>
+  );
+};
