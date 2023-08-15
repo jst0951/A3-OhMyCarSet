3ff1ee15772b33b93fa2383ef75831576fb2806d
jst0951 <jst0951@gmail.com>
2023-08-16 00:03:23 +0900

    feat: 외장 색상 동적 피드백 구현

--- backend/ohmycarset/src/main/java/com/softeer2nd/ohmycarset/service/SelectiveOptionService.java
+++ backend/ohmycarset/src/main/java/com/softeer2nd/ohmycarset/service/SelectiveOptionService.java
@@ -4,25 +4,62 @@ import com.softeer2nd.ohmycarset.domain.selective.OptionPackage;
 import com.softeer2nd.ohmycarset.domain.selective.PackageComponent;
 import com.softeer2nd.ohmycarset.domain.selective.RequiredOption;
 import com.softeer2nd.ohmycarset.dto.selectiveOptionDto.*;
+import com.softeer2nd.ohmycarset.repository.PurchaseHistoryRepository;
 import com.softeer2nd.ohmycarset.repository.SelectiveOptionRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class SelectiveOptionService {
 
     private final SelectiveOptionRepository selectiveOptionRepository;
+    private final PurchaseHistoryRepository purchaseHistoryRepository;
 
-    public SelectiveOptionService(SelectiveOptionRepository selectiveOptionRepository) {
+    public SelectiveOptionService(SelectiveOptionRepository selectiveOptionRepository, PurchaseHistoryRepository purchaseHistoryRepository) {
         this.selectiveOptionRepository = selectiveOptionRepository;
+        this.purchaseHistoryRepository = purchaseHistoryRepository;
     }
 
     public List<RequiredOptionDto> getAllOptionByName(String optionName) {
         List<RequiredOptionDto> requiredOptionDtoList = new ArrayList<>();
 
         List<RequiredOption> requiredOptionList = selectiveOptionRepository.findAllOptionByOptionName(optionName);
+
+        // 외장 색상 옵션인 경우 특수 처리 필요
+        if(optionName.equals("exterior_color")) {
+            // 각 색상 옵션에 대해 구매 건수를 조회합니다.
+            Map<RequiredOption, Long> purchaseCountMap = new HashMap<>();
+            for(RequiredOption eColorOption: requiredOptionList) {
+                Long purchaseCount = purchaseHistoryRepository.countByOptionNameAndOptionId(optionName, eColorOption.getId());
+                purchaseCountMap.put(eColorOption, purchaseCount);
+            }
+            // 내림차순으로 정렬합니다.
+            List<RequiredOption> sortedKeys = purchaseCountMap.entrySet().stream()
+                    .sorted(Map.Entry.<RequiredOption, Long>comparingByValue().reversed())
+                    .map(Map.Entry::getKey)
+                    .collect(Collectors.toList());
+            //각 순위에 따라 다르게 Feedback을 설정해줍니다.
+            for(int rank = 1; rank < sortedKeys.size() + 1; rank++) {
+                RequiredOption option = sortedKeys.get(rank - 1);
+                if(rank == 1) {
+                    option.setMainFeedback(String.format("%s 색상은 가장 많이 판매됐어요!", option.getName()));
+                    option.setSubFeedback("가장 인기있는 색상을 원하신다면, 탁월한 선택입니다.");
+                }
+                if(2 <= rank && rank <= 3) {
+                    option.setMainFeedback(String.format("%s 색상은 인기가 많아요!", option.getName()));
+                    option.setSubFeedback("인기있는 색상을 원하신다면, 탁월한 선택입니다.");
+                }
+                if(4 <= rank && rank <= 6) {
+                    option.setMainFeedback(String.format("%s 색상은 희소성이 있어요!", option.getName()));
+                    option.setSubFeedback("독특한 색상을 원하신다면, 탁월한 선택입니다.");
+                }
+            }
+        }
 
         for (RequiredOption requiredOption : requiredOptionList) {
             requiredOptionDtoList.add(new RequiredOptionDto(requiredOption));
