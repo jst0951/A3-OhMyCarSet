f7b4d9472909adc8ab7e2efc979f7ef78b700941
jst0951 <jst0951@gmail.com>
2023-08-16 17:15:40 +0900

    feat: [필수 옵션]옵션을 판매순위대로 정렬하는 로직을 추가하였습니다. 추가로, 외장 색상 전용 로직은 메서드로 분리하였습니다.

--- backend/ohmycarset/src/main/java/com/softeer2nd/ohmycarset/service/SelectiveOptionService.java
+++ backend/ohmycarset/src/main/java/com/softeer2nd/ohmycarset/service/SelectiveOptionService.java
@@ -26,46 +26,52 @@ public class SelectiveOptionService {
     }
 
     public List<RequiredOptionDto> getAllOptionByName(String optionName) {
-        List<RequiredOptionDto> requiredOptionDtoList = new ArrayList<>();
-
+        // 해당 카테고리의 모든 옵션 목록을 받아옵니다.
         List<RequiredOption> requiredOptionList = selectiveOptionRepository.findAllOptionByOptionName(optionName);
+
+        // 각 옵션에 대해 구매 건수를 조회합니다.
+        Map<RequiredOption, Long> purchaseCountMap = new HashMap<>();
+        for(RequiredOption eColorOption: requiredOptionList) {
+            Long purchaseCount = purchaseHistoryRepository.countByOptionNameAndOptionId(optionName, eColorOption.getId());
+            purchaseCountMap.put(eColorOption, purchaseCount);
+        }
+
+        // 내림차순으로 정렬합니다.
+        List<RequiredOption> sortedKeys = purchaseCountMap.entrySet().stream()
+                .sorted(Map.Entry.<RequiredOption, Long>comparingByValue().reversed())
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
 
         // 외장 색상 옵션인 경우 특수 처리 필요
         if(optionName.equals("exterior_color")) {
-            // 각 색상 옵션에 대해 구매 건수를 조회합니다.
-            Map<RequiredOption, Long> purchaseCountMap = new HashMap<>();
-            for(RequiredOption eColorOption: requiredOptionList) {
-                Long purchaseCount = purchaseHistoryRepository.countByOptionNameAndOptionId(optionName, eColorOption.getId());
-                purchaseCountMap.put(eColorOption, purchaseCount);
-            }
-            // 내림차순으로 정렬합니다.
-            List<RequiredOption> sortedKeys = purchaseCountMap.entrySet().stream()
-                    .sorted(Map.Entry.<RequiredOption, Long>comparingByValue().reversed())
-                    .map(Map.Entry::getKey)
-                    .collect(Collectors.toList());
-            //각 순위에 따라 다르게 Feedback을 설정해줍니다.
-            for(int rank = 1; rank < sortedKeys.size() + 1; rank++) {
-                RequiredOption option = sortedKeys.get(rank - 1);
-                if(rank == 1) {
-                    option.setMainFeedback(String.format("%s 색상은 가장 많이 판매됐어요!", option.getName()));
-                    option.setSubFeedback("가장 인기있는 색상을 원하신다면, 탁월한 선택입니다.");
-                }
-                if(2 <= rank && rank <= 3) {
-                    option.setMainFeedback(String.format("%s 색상은 인기가 많아요!", option.getName()));
-                    option.setSubFeedback("인기있는 색상을 원하신다면, 탁월한 선택입니다.");
-                }
-                if(4 <= rank && rank <= 6) {
-                    option.setMainFeedback(String.format("%s 색상은 희소성이 있어요!", option.getName()));
-                    option.setSubFeedback("독특한 색상을 원하신다면, 탁월한 선택입니다.");
-                }
-            }
+            handleExteriorColorOption(sortedKeys);
         }
 
-        for (RequiredOption requiredOption : requiredOptionList) {
+        List<RequiredOptionDto> requiredOptionDtoList = new ArrayList<>();
+        for (RequiredOption requiredOption : sortedKeys) {
             requiredOptionDtoList.add(new RequiredOptionDto(requiredOption));
         }
 
         return requiredOptionDtoList;
+    }
+
+    private void handleExteriorColorOption(List<RequiredOption> optionList) {
+        //각 순위에 따라 다르게 Feedback을 설정해줍니다.
+        for(int rank = 1; rank < optionList.size() + 1; rank++) {
+            RequiredOption option = optionList.get(rank - 1);
+            if(rank == 1) {
+                option.setMainFeedback(String.format("%s 색상은 가장 많이 판매됐어요!", option.getName()));
+                option.setSubFeedback("가장 인기있는 색상을 원하신다면, 탁월한 선택입니다.");
+            }
+            if(2 <= rank && rank <= 3) {
+                option.setMainFeedback(String.format("%s 색상은 인기가 많아요!", option.getName()));
+                option.setSubFeedback("인기있는 색상을 원하신다면, 탁월한 선택입니다.");
+            }
+            if(4 <= rank && rank <= 6) {
+                option.setMainFeedback(String.format("%s 색상은 희소성이 있어요!", option.getName()));
+                option.setSubFeedback("독특한 색상을 원하신다면, 탁월한 선택입니다.");
+            }
+        }
     }
 
     public List<OptionPackageDto> getAllPackageByName(String packageName) {
