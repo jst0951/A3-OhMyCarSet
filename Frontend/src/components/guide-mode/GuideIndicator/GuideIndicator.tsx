4e649d6ba61fb5ada844cf18da11253d0fc3912d
yen001004 <yenn001004@gmail.com>
2023-08-16 17:51:20 +0900

    feat: 가이드 태그 선택 애니메이션

--- Frontend/src/components/guide-mode/GuideIndicator/GuideIndicator.tsx
+++ Frontend/src/components/guide-mode/GuideIndicator/GuideIndicator.tsx
@@ -1,9 +1,12 @@
 import * as S from './GuideIndicator.style';
 import guideDescriptionData from '@/asset/data/guideDescriptionData.json';
 import { useGuideModeContext } from '@/contexts/GuideModeProvider';
+import { useEffect, useState } from 'react';
 
 export default function GuideIndicator() {
   const { GuideModeStep, setGuideModeStep } = useGuideModeContext();
+  const [prevId, setPrevId] = useState(1);
+  const [nextId, setNextId] = useState(1);
 
   const handleClick = (page: number) => {
     if (GuideModeStep > page) {
@@ -11,10 +14,21 @@ export default function GuideIndicator() {
     }
   };
 
+  useEffect(() => {
+    setPrevId(GuideModeStep - 1);
+    setNextId(GuideModeStep);
+  }, [GuideModeStep]);
+
   return (
     <S.IndicatorContainer>
       {guideDescriptionData.map((data) => (
-        <S.Indicator key={data.page} $selected={data.page === GuideModeStep} onClick={() => handleClick(data.page)}>
+        <S.Indicator
+          key={data.page}
+          $id={data.page}
+          $isPrev={prevId === data.page}
+          $isNext={nextId === data.page}
+          onClick={() => handleClick(data.page)}
+        >
           {data.page}
         </S.Indicator>
       ))}
--- Frontend/src/components/guide-mode/GuideMainTag/GuideMainTag.style.tsx
+++ Frontend/src/components/guide-mode/GuideMainTag/GuideMainTag.style.tsx
@@ -4,6 +4,7 @@ export const MainContainer = styled.div`
   display: flex;
   flex-direction: row;
   height: calc(100vh - 110px);
+  min-height: 600px;
   align-items: center;
   justify-content: space-between;
   width: 1024px;
