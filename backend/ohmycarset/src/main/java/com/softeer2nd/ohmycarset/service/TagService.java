cbb2b85f993f037b9cbc06e460adfd4fdf7d20c5
jst0951 <jst0951@gmail.com>
2023-08-16 11:23:38 +0900

    fix: 옵션 패키지에서도 RequiredOption 객체를 이용하던 오류를 OptionPackage를 이용하도록 수정

--- backend/ohmycarset/src/main/java/com/softeer2nd/ohmycarset/service/TagService.java
+++ backend/ohmycarset/src/main/java/com/softeer2nd/ohmycarset/service/TagService.java
@@ -1,6 +1,7 @@
 package com.softeer2nd.ohmycarset.service;
 
 import com.softeer2nd.ohmycarset.domain.Tag;
+import com.softeer2nd.ohmycarset.domain.selective.OptionPackage;
 import com.softeer2nd.ohmycarset.domain.selective.RequiredOption;
 import com.softeer2nd.ohmycarset.dto.SelectiveOptionTagDto.SelectiveOptionTagDto;
 import com.softeer2nd.ohmycarset.dto.SelectiveOptionTagDto.TagDto;
@@ -47,21 +48,21 @@ public class TagService {
 
     public List<SelectiveOptionTagDto> getPurchaseTagByPackageName(String packageName) {
         // 각 카운트 조회
-        Map<RequiredOption, Long> purchaseCountMap = new HashMap<>();
+        Map<OptionPackage, Long> purchaseCountMap = new HashMap<>();
 
-        List<RequiredOption> optionList = selectiveOptionRepository.findAllOptionByOptionName(packageName);
-        for(RequiredOption option: optionList) {
-            Long purchaseCount = purchaseHistoryRepository.countByPackageNameAndOptionId(packageName, option.getId());
-            purchaseCountMap.put(option, purchaseCount);
+        List<OptionPackage> optionPackageList = selectiveOptionRepository.findAllPackageByPackageName(packageName);
+        for(OptionPackage optionPackage: optionPackageList) {
+            Long purchaseCount = purchaseHistoryRepository.countByPackageNameAndOptionId(packageName, optionPackage.getId());
+            purchaseCountMap.put(optionPackage, purchaseCount);
         }
 
         // 확률 계산 및 태그로 전송
         List<SelectiveOptionTagDto> selectiveOptionTagDtoList = new ArrayList<>();
 
         Long purchaseCountSum = purchaseCountMap.values().stream().mapToLong(Long::longValue).sum();
-        for(RequiredOption option: optionList) {
-            Double purchasePercentage = (double) purchaseCountMap.get(option) / purchaseCountSum * 100;
-            selectiveOptionTagDtoList.add(new SelectiveOptionTagDto(option, purchasePercentage));
+        for(OptionPackage optionPackage: optionPackageList) {
+            Double purchasePercentage = (double) purchaseCountMap.get(optionPackage) / purchaseCountSum * 100;
+            selectiveOptionTagDtoList.add(new SelectiveOptionTagDto(optionPackage, purchasePercentage));
         }
 
         return selectiveOptionTagDtoList;
@@ -138,17 +139,17 @@ public class TagService {
             tagList.add(tagRepository.findByName(tagName).orElse(null));
         }
 
-        // 본 카테고리의 모든 옵션의 목록을 가져옵니다.
-        List<RequiredOption> optionList = selectiveOptionRepository.findAllOptionByOptionName(packageName);
+        // 본 카테고리의 모든 옵션 패키지의 목록을 가져옵니다.
+        List<OptionPackage> optionPackageList = selectiveOptionRepository.findAllPackageByPackageName(packageName);
 
         // 모든 부가 정보들을 담을 목록을 만듭니다.
         List<SelectiveOptionTagDto> optionTagDtoList = new ArrayList<>();
 
         // 각 옵션에 대해 구매비율, 유사도 및 태그 목록을 구합니다.
         Long totalPurchaseCount = purchaseHistoryRepository.count(); // 구매비율 연산용
-        for (RequiredOption option : optionList) {
+        for (OptionPackage optionPackage : optionPackageList) {
             // 구매비율
-            Long optionPurchaseCount = purchaseHistoryRepository.countByPackageNameAndOptionId(packageName, option.getId());
+            Long optionPurchaseCount = purchaseHistoryRepository.countByPackageNameAndOptionId(packageName, optionPackage.getId());
             Double purchaseRate = (double) optionPurchaseCount / totalPurchaseCount * 100;
 
             // TODO 유사도 연산 로직
@@ -159,7 +160,7 @@ public class TagService {
 
             // 유저가 선택한 태그와 옵션에 정의된 태그 중 겹치는 태그만 연산합니다.
             List<Tag> intersectionTagList = new ArrayList<>();
-            List<Tag> optionTagList = tagRepository.findAllByOptionNameAndOptionId(packageName, option.getId());
+            List<Tag> optionTagList = tagRepository.findAllByOptionNameAndOptionId(packageName, optionPackage.getId());
             for (Tag tag : tagList) {
                 for (Tag optionTag : optionTagList) {
                     if (Objects.equals(tag.getId(), optionTag.getId())) {
@@ -174,7 +175,7 @@ public class TagService {
                 Long purchaseCount = purchaseHistoryRepository.countByTagId(tag.getId());
 
                 // 해당 태그를 선택하고, 해당 옵션을 구매한 내역 수를 받아옵니다.
-                Long intersectionPurchaseCount = purchaseHistoryRepository.countByTagIdAndPackageNameAndOptionId(tag.getId(), packageName, option.getId());
+                Long intersectionPurchaseCount = purchaseHistoryRepository.countByTagIdAndPackageNameAndOptionId(tag.getId(), packageName, optionPackage.getId());
 
                 // 선택 비율을 연산합니다.
                 Double selectionPercentage = (double) intersectionPurchaseCount / purchaseCount * 100;
@@ -187,7 +188,7 @@ public class TagService {
             tagDtoList.sort((t0, t1) -> Double.compare(t1.getPercentage(), t0.getPercentage()));
 
             // 목록에 담습니다.
-            optionTagDtoList.add(new SelectiveOptionTagDto(option, purchaseRate, similarity, tagDtoList));
+            optionTagDtoList.add(new SelectiveOptionTagDto(optionPackage, purchaseRate, similarity, tagDtoList));
         }
 
         return optionTagDtoList;
