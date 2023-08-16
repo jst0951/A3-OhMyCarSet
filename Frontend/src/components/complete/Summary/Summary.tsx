71478135d247e75e05a1b764374516a55c9fa486
hb9901 <hb9901@gmail.com>
2023-08-16 17:50:44 +0900

    feat: summary에 휠, 옵션 추가

--- Frontend/src/components/complete/Summary/Summary.tsx
+++ Frontend/src/components/complete/Summary/Summary.tsx
@@ -3,28 +3,50 @@ import { useSelectOptionState } from '@/contexts/SelectOptionProvider';
 import { DEFAULT_PRICE } from '@/constants';
 import SummaryItem from './SummaryItem/SummaryItem';
 import { SectionProps } from '@/components/self-mode/SelfModeMain/Estimate/Estimate';
+import { useSelectPackageState } from '@/contexts/SelectPackageProvider';
 
 export default function Summary() {
   const selectOptionState = useSelectOptionState();
+  const selectPackageState = useSelectPackageState();
+  const tempTotal = selectOptionState.totalPrice + selectPackageState.totalPrice;
 
   const sectionList: SectionProps[] = [
     {
       sectionTitle: '팰리세이드 Le Blanc (르블랑)',
       sectionTotal: DEFAULT_PRICE,
-      subList: [0, 1, 2, 5].map((idx) => ({
-        stepName: selectOptionState.dataList[idx].stepName,
-        selectName: selectOptionState.dataList[idx].selectedName,
-        price: selectOptionState.dataList[idx].price,
-      })),
+      subList: [0, 1, 2].map((idx) => {
+        const data = selectOptionState.dataList[idx];
+        return { stepName: data.stepName, selectName: data.selectedName, price: data.price };
+      }),
     },
     {
       sectionTitle: '색상',
       sectionTotal: (selectOptionState.dataList[3]?.price ?? 0) + (selectOptionState.dataList[4]?.price ?? 0),
-      subList: [3, 4].map((idx) => ({
-        stepName: selectOptionState.dataList[idx].stepName,
-        selectName: selectOptionState.dataList[idx].selectedName,
-        price: selectOptionState.dataList[idx].price,
-      })),
+      subList: [3, 4].map((idx) => {
+        const data = selectOptionState.dataList[idx];
+        return { stepName: data.stepName, selectName: data.selectedName, price: data.price };
+      }),
+    },
+    {
+      sectionTitle: '휠',
+      sectionTotal: selectOptionState.dataList[5]?.price ?? 0,
+      subList: [5].map((idx) => {
+        const data = selectOptionState.dataList[idx];
+        return { stepName: data.stepName, selectName: data.selectedName, price: data.price };
+      }),
+    },
+    {
+      sectionTitle: '선택',
+      sectionTotal: selectPackageState.totalPrice,
+      subList: Array.from(selectPackageState.packageList)
+        .map((packageData) =>
+          Array.from(packageData.selectedList.values()).map((optionData) => ({
+            stepName: '선택 옵션',
+            selectName: optionData.name,
+            price: optionData.price,
+          }))
+        )
+        .flat(),
     },
   ];
 
@@ -35,7 +57,7 @@ export default function Summary() {
           <S.Title>견적 요약</S.Title>
           <S.rightTitle>
             <S.subTitle>차량 총 견적 금액</S.subTitle>
-            <S.TotalPrice>{selectOptionState.totalPrice.toLocaleString()} 원</S.TotalPrice>
+            <S.TotalPrice>{tempTotal.toLocaleString()} 원</S.TotalPrice>
           </S.rightTitle>
         </S.TitleContainer>
         <S.SectionContainer>
