0b212bb71ebfe156a93da29e23ec8a1d9f577ccd
yen001004 <yenn001004@gmail.com>
2023-08-16 15:55:59 +0900

    feat: GuideMain 수정

--- Frontend/src/components/guide-mode/GuideModeMain/GuideModeMain.tsx
+++
@@ -1,9 +0,0 @@
-import GuideMainTag from '@/components/guide-mode/GuideMainTag/GuideMainTag';
-import GuideMainComplete from '../GuideMainComplete/GuideMainComplete';
-import { useState } from 'react';
-
-export default function GuideMain() {
-  const [complete, setComplete] = useState<boolean>(false);
-
-  return <>{complete ? <GuideMainComplete /> : <GuideMainTag setComplete={setComplete} />}</>;
-}
