25f8db932f9a3cc6e9cb1e54a295c84cd1897994
yen001004 <yenn001004@gmail.com>
2023-08-16 14:19:32 +0900

    fix: conflict 해결

--- Frontend/src/components/guide-mode/GuideModeMain/GuideModeMain.tsx
+++ Frontend/src/components/guide-mode/GuideModeMain/GuideModeMain.tsx
@@ -1,37 +1,9 @@
-import * as S from './GuideModeMain.style';
-import GuideModeIndicator from './GuideModeIndicator/GuideModeIndicator';
-import GuideModeSingleTag from './GuideModeSingleTag/GuideModeSingleTag';
-import { useGuideModeContext } from '@/contexts/GuideModeProvider';
-import guideDescriptionData from '@/asset/data/guideDescriptionData.json';
-import GuideModeMultiTag from './GuideModeMultiTag/GuideModeMultiTag';
-import RectButton from '@/components/common/button/RectButton/RectButton';
+import GuideMainTag from '@/components/guide-mode/GuideMainTag/GuideMainTag';
+import GuideMainComplete from '../GuideMainComplete/GuideMainComplete';
 import { useState } from 'react';
 
-export default function GuideModeMain() {
-  const { GuideModeStep } = useGuideModeContext();
-  const currentIndex = GuideModeStep - 1;
+export default function GuideMain() {
   const [complete, setComplete] = useState<boolean>(false);
 
-  return (
-    <>
-      <S.MainContainer>
-        <S.MainLeftContainer>
-          <GuideModeIndicator />
-          <S.MainDescription>{guideDescriptionData[currentIndex].mainDescription}</S.MainDescription>
-          <S.MainDescriptionBold>{guideDescriptionData[currentIndex].mainDescriptionBold}</S.MainDescriptionBold>
-          <S.SubDescription>{guideDescriptionData[currentIndex].subDescription}</S.SubDescription>
-
-          <S.ButtonContainer $hidden={!complete}>
-            <RectButton type="recommended" page="guide">
-              선택 완료
-            </RectButton>
-          </S.ButtonContainer>
-        </S.MainLeftContainer>
-
-        <S.MainRightContainer>
-          {currentIndex === 2 ? <GuideModeMultiTag setComplete={setComplete} /> : <GuideModeSingleTag />}
-        </S.MainRightContainer>
-      </S.MainContainer>
-    </>
-  );
+  return <>{complete ? <GuideMainComplete /> : <GuideMainTag setComplete={setComplete} />}</>;
 }
