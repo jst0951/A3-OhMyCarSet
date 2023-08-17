0b847597a3175f8a18e790e34100433059c13b45
jst0951 <jst0951@gmail.com>
2023-08-17 17:51:03 +0900

    fix: PackageComponentDTO에 잘못 붙었던 purchaseRate, tags를 OptionPackageDto로 이동

--- backend/ohmycarset/src/main/java/com/softeer2nd/ohmycarset/dto/selectiveOptionDto/PackageComponentDto.java
+++ backend/ohmycarset/src/main/java/com/softeer2nd/ohmycarset/dto/selectiveOptionDto/PackageComponentDto.java
@@ -13,27 +13,13 @@ public class PackageComponentDto {
     private final String name;
     private final String description;
     private final String imgSrc;
-    private final Double purchaseRate;
-    private final List<TagDto> tags;
 
     @ConstructorProperties({"id", "name", "description", "imgSrc", "purchaseRate", "tags"})
-    public PackageComponentDto(Long id, String name, String description, String imgSrc, Double purchaseRate, List<TagDto> tags) {
+    public PackageComponentDto(Long id, String name, String description, String imgSrc) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.imgSrc = imgSrc;
-        this.purchaseRate = purchaseRate;
-        this.tags = tags;
-    }
-
-    // 셀프모드/가이드모드에서 선택 옵션들을 제공받는 경우 이 생성자를 이용합니다.
-    public PackageComponentDto(PackageComponent component, Double purchaseRate, List<TagDto> tagDtoList) {
-        this.id = component.getId();
-        this.name = component.getName();
-        this.description = component.getDescription();
-        this.imgSrc = component.getImgSrc();
-        this.purchaseRate = purchaseRate;
-        this.tags = tagDtoList;
     }
 
     // /recommend를 통해 응답을 받는 경우 이 생성자를 이용합니다.
@@ -42,7 +28,5 @@ public class PackageComponentDto {
         this.name = component.getName();
         this.description = component.getDescription();
         this.imgSrc = component.getImgSrc();
-        this.purchaseRate = null;
-        this.tags = null;
     }
 }
