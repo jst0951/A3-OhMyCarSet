1387945828aadad9e9bad0078750ed7a2f6c45bc
Jaehong Kim <kjhonggg95@gmail.com>
2023-08-17 16:55:48 +0900

    feat: 외장/내장색상 태그 계산

--- backend/ohmycarset/src/main/java/com/softeer2nd/ohmycarset/service/TagService.java
+++ backend/ohmycarset/src/main/java/com/softeer2nd/ohmycarset/service/TagService.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Service
 public class TagService {
@@ -74,61 +73,131 @@ public class TagService {
         return selectiveOptionTagDtoList;
     }
 
-    public List<SelectiveOptionTagDto> getKeywordTagRequiredOption(UserInfoDto userInfoDto, String optionName) {
+    public List<SelectiveOptionTagDto> getKeywordTagRequiredOption(UserInfoDto userInfoDto, String categoryName) {
         List<SelectiveOptionTagDto> response = new ArrayList<>();
-
-        // 성별, 나이, 태그 id
         Character gender = userInfoDto.getGender();
         Integer age = userInfoDto.getAge();
-        List<Long> tagIds = Stream.of(userInfoDto.getTag1(), userInfoDto.getTag2(), userInfoDto.getTag3())
-                .map(tagName -> tagRepository.findByName(tagName).orElseThrow().getId())
-                .collect(Collectors.toList());
+        Long optionId = 1L; // 추천된 옵션id
 
-        // 옵션id, 구매내역수 - 구매내역 수 내림차순으로 정렬
-        List<PurchaseCountDto> purchaseCountDtoList = purchaseHistoryRepository.countByOptionNameAndGenderAndAgeAndTags(optionName, gender, age, tagIds)
-                .stream()
-                .sorted(Comparator.comparing(PurchaseCountDto::getCount).reversed())
-                .collect(Collectors.toList());
+        if (categoryName.equals("powertrain") || categoryName.equals("wd") || categoryName.equals("body")) {
+            RequiredOption recommendedOption = selectiveOptionRepository.findOptionByOptionIdAndOptionName(optionId, categoryName);
 
-        // tag dto 생성
-        List<TagDto> tagDtoList = new ArrayList<>();
+            // 키워드 포함
+            List<Long> tagIds = tagRepository.findIdsByNames(new ArrayList<>(Arrays.asList(userInfoDto.getTag1(), userInfoDto.getTag2(), userInfoDto.getTag3())));
+            List<PurchaseCountDto> purchaseCountDtoList = purchaseHistoryRepository.countByCategoryNameAndGenderAndAgeAndTags(categoryName, gender, age, tagIds);
+            Long totalSimilarCount = purchaseCountDtoList.stream().mapToLong(PurchaseCountDto::getCount).sum();
 
-        // 총 유사유저 수
-        Long totalSimilarUserCount = purchaseHistoryRepository.countByGenderAndAgeAndTags(gender, age, tagIds);
 
-        // 유사유저 정보
-        RequiredOption mostSimilarOption = selectiveOptionRepository.findOptionByOptionIdAndOptionName(purchaseCountDtoList.get(0).getOptionId(), optionName);
-
-        // 태그 교집합
-        List<Tag> optionTags = tagRepository.findAllByOptionNameAndOptionId(optionName, mostSimilarOption.getId());
-        List<String> userTagNames = Arrays.asList(userInfoDto.getTag1(), userInfoDto.getTag2(), userInfoDto.getTag3());
-        List<Tag> intersectTags = optionTags.stream()
-                .filter(tag -> userTagNames.contains(tag.getName()))
-                .collect(Collectors.toList());
-
-        for (Tag tag : intersectTags) {
-            Double percentage = (double) purchaseHistoryRepository.countByTagIdAndOptionNameAndOptionId(tag.getId(), optionName, mostSimilarOption.getId()) / purchaseHistoryRepository.countByTagId(tag.getId()) * 100;
-            tagDtoList.add(new TagDto(tag.getId(), tag.getName(), percentage));
+            // 키워드 포함x - 연령대+성별
         }
-        tagDtoList.sort(Comparator.comparing(TagDto::getPercentage).reversed());
 
-        response.add(new SelectiveOptionTagDto(mostSimilarOption.getId(), mostSimilarOption.getName(), (double) purchaseCountDtoList.get(0).getCount() / totalSimilarUserCount * 100, tagDtoList));
+        if(categoryName.equals("wheel")) {
 
-        // 전체 판매량
-        Long totalPurchaseCount = purchaseHistoryRepository.count();
+        }
 
-        // 구매비율 정보
-        purchaseCountDtoList.subList(1, purchaseCountDtoList.size())
-                .stream()
-                .sorted(Comparator.comparing(dto -> {
-                    Long purchaseCount = purchaseHistoryRepository.countByOptionNameAndOptionId(optionName, dto.getOptionId());
-                    return -purchaseCount; // 내림차순으로 정렬하기 위해 음수로 변환
-                }))
-                .forEach(dto -> {
-                    Long purchaseCount = purchaseHistoryRepository.countByOptionNameAndOptionId(optionName, dto.getOptionId());
-                    RequiredOption option = selectiveOptionRepository.findOptionByOptionIdAndOptionName(dto.getOptionId(), optionName);
-                    response.add(new SelectiveOptionTagDto(option.getId(), option.getName(), (double) purchaseCount / totalPurchaseCount * 100, null));
-                });
+        // 색상 - 연령대+성별 기준 제일 많이 팔린 것 선택
+        if (categoryName.equals("exterior_color") || categoryName.equals("interior_color")) {
+            List<PurchaseCountDto> purchaseCountDtoList = purchaseHistoryRepository.findAllByCategoryNameAndGenderAndAge(categoryName, gender, age);
+
+            /**
+             * 추천 옵션 - 유사유저 구매 비율
+             */
+            Long totalSimilarCount = purchaseCountDtoList.stream().mapToLong(PurchaseCountDto::getCount).sum();
+            RequiredOption recommendedOption = selectiveOptionRepository.findOptionByOptionIdAndOptionName(purchaseCountDtoList.get(0).getOptionId(), categoryName);
+            Double genderRatio = purchaseHistoryRepository.countByCategoryNameAndOptionIdAndGender(categoryName, recommendedOption.getId(), gender);
+            Double ageRatio = purchaseHistoryRepository.countByCategoryNameAndOptionIdAndAge(categoryName, recommendedOption.getId(), age);
+            List<TagDto> tagDtoList = Arrays.asList(new TagDto(1L, "성별", genderRatio), new TagDto(2L, "나이대", ageRatio));
+
+            response.add(new SelectiveOptionTagDto(
+                    recommendedOption.getId(),
+                    recommendedOption.getName(),
+                    (double) purchaseCountDtoList.get(0).getCount() / totalSimilarCount * 100,
+                    tagDtoList));
+
+            /**
+             * 나머지 옵션 - 총 구매비율
+             */
+            Long totalCount = purchaseHistoryRepository.count();
+            List<Long> optionIds = purchaseCountDtoList.subList(1, purchaseCountDtoList.size()).stream()
+                    .map(PurchaseCountDto::getOptionId)
+                    .collect(Collectors.toList());
+
+            List<Long> counts = purchaseHistoryRepository.countByCategoryNameAndOptionIds(categoryName, optionIds);
+
+            int idx = 0;
+            List<RequiredOption> options = selectiveOptionRepository.findOptionsByOptionIdsAndCategoryName(optionIds, categoryName);
+            for (RequiredOption option : options) {
+                response.add(new SelectiveOptionTagDto(
+                        option.getId(),
+                        option.getName(),
+                        (double) counts.get(idx++) / totalCount * 100,
+                        null
+                ));
+            }
+
+            return response;
+        }
+
+
+//
+//        List<Long> tagIds = tagRepository.findIdsByNames(new ArrayList<>(Arrays.asList(userInfoDto.getTag1(), userInfoDto.getTag2(), userInfoDto.getTag3())));
+//
+//        // 옵션id, 구매내역수 - 구매내역 수 내림차순으로 정렬
+//        List<PurchaseCountDto> purchaseCountDtoList = purchaseHistoryRepository.countByCategoryNameAndGenderAndAgeAndTags(categoryName, gender, age, tagIds)
+//                .stream()
+//                .sorted(Comparator.comparing(PurchaseCountDto::getCount).reversed())
+//                .collect(Collectors.toList());
+//
+//        // 총 유사유저 수
+//        Long totalSimilarUserCount = purchaseCountDtoList.stream()
+//                .mapToLong(PurchaseCountDto::getCount)
+//                .sum();
+//
+//        System.out.println("totalSimilarUserCount = " + totalSimilarUserCount);
+//
+//        // 유사유저 정보
+//        RequiredOption mostSimilarOption = selectiveOptionRepository.findOptionByOptionIdAndOptionName(purchaseCountDtoList.get(0).getOptionId(), categoryName);
+//
+//        // 태그 교집합
+//        List<Tag> optionTags = tagRepository.findAllByOptionNameAndOptionId(categoryName, mostSimilarOption.getId());
+//        List<String> userTagNames = Arrays.asList(userInfoDto.getTag1(), userInfoDto.getTag2(), userInfoDto.getTag3());
+//        List<Tag> intersectTags = optionTags.stream()
+//                .filter(tag -> userTagNames.contains(tag.getName()))
+//                .collect(Collectors.toList());
+//
+//        // tag dto 생성
+//        List<TagDto> tagDtoList = new ArrayList<>();
+//
+//        for (Tag tag : intersectTags) {
+//            Double percentage = (double) purchaseHistoryRepository.countByTagIdAndOptionNameAndOptionId(tag.getId(), categoryName, mostSimilarOption.getId()) / purchaseHistoryRepository.countByTagId(tag.getId()) * 100;
+//            tagDtoList.add(new TagDto(tag.getId(), tag.getName(), percentage));
+//        }
+//        tagDtoList.sort(Comparator.comparing(TagDto::getPercentage).reversed());
+//
+//        response.add(new SelectiveOptionTagDto(mostSimilarOption.getId(), mostSimilarOption.getName(), (double) purchaseCountDtoList.get(0).getCount() / totalSimilarUserCount * 100, tagDtoList));
+//
+//        // 전체 판매량
+//        Long totalPurchaseCount = purchaseHistoryRepository.count();
+//
+//        // 구매비율 정보
+//        purchaseCountDtoList.subList(1, purchaseCountDtoList.size())
+//                .stream()
+//                .sorted(Comparator.comparing(dto -> {
+//                    Long purchaseCount = purchaseHistoryRepository.countByOptionNameAndOptionId(categoryName, dto.getOptionId());
+//                    return -purchaseCount; // 내림차순으로 정렬하기 위해 음수로 변환
+//                }))
+//                .forEach(dto -> {
+//                    Long purchaseCount = purchaseHistoryRepository.countByOptionNameAndOptionId(categoryName, dto.getOptionId());
+//                    RequiredOption option = selectiveOptionRepository.findOptionByOptionIdAndOptionName(dto.getOptionId(), categoryName);
+//                    response.add(new SelectiveOptionTagDto(option.getId(), option.getName(), (double) purchaseCount / totalPurchaseCount * 100, null));
+//                });
+//
+//        for (PurchaseCountDto dto : purchaseCountDtoList.subList(1, purchaseCountDtoList.size())) {
+//            Long purchaseCount = purchaseHistoryRepository.countByOptionNameAndOptionId(categoryName, dto.getOptionId());
+//            RequiredOption option = selectiveOptionRepository.findOptionByOptionIdAndOptionName(dto.getOptionId(), categoryName);
+//            response.add(new SelectiveOptionTagDto(option.getId(), option.getName(), (double)purchaseCount / totalPurchaseCount * 100, null));
+//        }
+
 
         return response;
     }
