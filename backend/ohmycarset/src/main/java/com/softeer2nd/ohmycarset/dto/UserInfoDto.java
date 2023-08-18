9b1421f2a95cc3f346c81799f91922788d871669
Jaehong Kim <kjhonggg95@gmail.com>
2023-08-18 21:30:56 +0900

    feat: UserInfoDto 필드 접근자 변경
    public -> private

--- backend/ohmycarset/src/main/java/com/softeer2nd/ohmycarset/dto/UserInfoDto.java
+++ backend/ohmycarset/src/main/java/com/softeer2nd/ohmycarset/dto/UserInfoDto.java
@@ -6,11 +6,11 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class UserInfoDto {
-    public Integer age;
-    public Character gender;
-    public String tag1;
-    public String tag2;
-    public String tag3;
+    private Integer age;
+    private Character gender;
+    private String tag1;
+    private String tag2;
+    private String tag3;
 
     public UserInfoDto(Integer age, Character gender, String tag1, String tag2, String tag3) {
         this.age = age;
