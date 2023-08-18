59c7ca363f0e66e75356f2727e8a2641a8d92faa
Jaehong Kim <kjhonggg95@gmail.com>
2023-08-19 00:48:47 +0900

    remove: 구매율, 태그 필드 및 생성자 제거
    PackageComponent 에는 구매율, 태그 명시 x

--- backend/ohmycarset/src/main/java/com/softeer2nd/ohmycarset/dto/selectiveOptionDto/PackageComponentDto.java
+++ backend/ohmycarset/src/main/java/com/softeer2nd/ohmycarset/dto/selectiveOptionDto/PackageComponentDto.java
@@ -12,26 +12,11 @@ public class PackageComponentDto {
     private final String name;
     private final String description;
     private final String imgSrc;
-    private final Double purchaseRate;
-    private final List<TagDto> tags;
 
-    // 셀프모드/가이드모드에서 선택 옵션들을 제공받는 경우 이 생성자를 이용합니다.
-    public PackageComponentDto(PackageComponent component, Double purchaseRate, List<TagDto> tagDtoList) {
-        this.id = component.getId();
-        this.name = component.getName();
-        this.description = component.getDescription();
-        this.imgSrc = component.getImgSrc();
-        this.purchaseRate = purchaseRate;
-        this.tags = tagDtoList;
-    }
-
-    // /recommend를 통해 응답을 받는 경우 이 생성자를 이용합니다.
     public PackageComponentDto(PackageComponent component) {
         this.id = component.getId();
         this.name = component.getName();
         this.description = component.getDescription();
         this.imgSrc = component.getImgSrc();
-        this.purchaseRate = null;
-        this.tags = null;
     }
 }
