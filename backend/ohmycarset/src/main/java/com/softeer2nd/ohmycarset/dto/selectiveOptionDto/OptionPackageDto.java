d606858ceb5d0d70d1885ee3168c5ef5a2763171
Jaehong Kim <kjhonggg95@gmail.com>
2023-08-18 22:29:23 +0900

    feat: 가이드모드 엔드포인트에서도 사용할 수 있도록 필드 및 생성자 추가

--- backend/ohmycarset/src/main/java/com/softeer2nd/ohmycarset/dto/selectiveOptionDto/OptionPackageDto.java
+++ backend/ohmycarset/src/main/java/com/softeer2nd/ohmycarset/dto/selectiveOptionDto/OptionPackageDto.java
@@ -2,6 +2,7 @@ package com.softeer2nd.ohmycarset.dto.selectiveOptionDto;
 
 import com.softeer2nd.ohmycarset.domain.selective.OptionPackage;
 import com.softeer2nd.ohmycarset.domain.selective.PackageComponent;
+import com.softeer2nd.ohmycarset.dto.SelectiveOptionTagDto.TagDto;
 import lombok.Getter;
 
 import java.util.ArrayList;
@@ -14,6 +15,22 @@ public class OptionPackageDto {
     private final Integer price;
     private final String iconSrc;
     private final List<PackageComponentDto> components;
+    private final Double purchaseRate;
+    private final List<TagDto> tags;
+
+    public OptionPackageDto(OptionPackage optionPackage, List<PackageComponent> componentList, Double purchaseRate, List<TagDto> tags) {
+        this.id = optionPackage.getId();
+        this.name = optionPackage.getName();
+        this.price = optionPackage.getPrice();
+        this.iconSrc = optionPackage.getIconSrc();
+        List<PackageComponentDto> componentDtoList = new ArrayList<>();
+        for(PackageComponent systemOptionComponent: componentList) {
+            componentDtoList.add(new PackageComponentDto(systemOptionComponent));
+        }
+        this.components = componentDtoList;
+        this.purchaseRate = purchaseRate;
+        this.tags = tags;
+    }
 
     public OptionPackageDto(OptionPackage optionPackage, List<PackageComponent> componentList) {
         this.id = optionPackage.getId();
@@ -25,5 +42,7 @@ public class OptionPackageDto {
             componentDtoList.add(new PackageComponentDto(systemOptionComponent));
         }
         this.components = componentDtoList;
+        this.purchaseRate = null;
+        this.tags = null;
     }
 }
