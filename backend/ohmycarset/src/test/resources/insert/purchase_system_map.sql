c49c0aa93ce20988995cd2a7cb720a7ed38e5415
jst0951 <jst0951@gmail.com>
2023-08-20 04:17:50 +0900

    feat: 테스트를 위한 데이터 추가

--- backend/ohmycarset/src/test/resources/insert/purchase_system_map.sql
+++ backend/ohmycarset/src/test/resources/insert/purchase_system_map.sql
@@ -1,2 +1,3 @@
 INSERT INTO ohmycarset.purchase_system_map (id, purchase_id, option_id) VALUES (1, 6, 2);
 INSERT INTO ohmycarset.purchase_system_map (id, purchase_id, option_id) VALUES (2, 8, 1);
+INSERT INTO ohmycarset.purchase_system_map (id, purchase_id, option_id) VALUES (3, 9, 1);
