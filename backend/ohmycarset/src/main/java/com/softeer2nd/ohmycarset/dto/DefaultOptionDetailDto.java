2e755c7e49d7b5724ea62f921a03dd5e7175cd6f
Jaehong Kim <kjhonggg95@gmail.com>
2023-08-20 23:58:02 +0900

    feat: 기본포함품목에 설명 필드 추가

--- backend/ohmycarset/src/main/java/com/softeer2nd/ohmycarset/dto/DefaultOptionDetailDto.java
+++ backend/ohmycarset/src/main/java/com/softeer2nd/ohmycarset/dto/DefaultOptionDetailDto.java
@@ -7,11 +7,13 @@ import lombok.Getter;
 public class DefaultOptionDetailDto {
     private final Long optionId;
     private final String optionName;
+    private final String description;
     private final String imgSrc;
 
     public DefaultOptionDetailDto(DefaultOption defaultOption) {
         this.optionId = defaultOption.getId();
         this.optionName = defaultOption.getName();
+        this.description = defaultOption.getDescription();
         this.imgSrc = defaultOption.getImgSrc();
     }
 }
