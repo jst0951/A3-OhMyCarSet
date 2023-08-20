d56db48918c99256a89d0d7963b35c4b9a1a68cd
yen001004 <yenn001004@gmail.com>
2023-08-21 02:25:57 +0900

    feat: 스텝 초기화

--- Frontend/src/pages/MainPage/MainPage.tsx
+++ Frontend/src/pages/MainPage/MainPage.tsx
@@ -2,8 +2,15 @@ import LandingMain from '@/components/main/LandingMain/LandingMain';
 import LandingDetail from '@/components/main/LandingMainDetail/LandingMainDetail';
 import Layout from '../../components/layout/Layout';
 import * as S from './MainPage.style';
+import { useEffect } from 'react';
+import { useSelfModeContext } from '@/contexts/SelfModeProvider';
 
 export default function MainPage() {
+  const { setSelfModeStep } = useSelfModeContext();
+  useEffect(() => {
+    setSelfModeStep(1);
+  }, []);
+
   return (
     <>
       <Layout>
