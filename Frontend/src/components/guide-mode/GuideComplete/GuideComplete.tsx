09d38aa20fb09136032cb8a3d0a4e82a76e53990
yen001004 <yenn001004@gmail.com>
2023-08-21 11:06:21 +0900

    feat: 가이드 complete 이미지 추가

--- Frontend/src/components/guide-mode/GuideComplete/GuideComplete.tsx
+++ Frontend/src/components/guide-mode/GuideComplete/GuideComplete.tsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import { guideStepT } from '../GuideMain/GuideMain';
 import { Dispatch } from 'react';
 import { COMPLETE_URL } from '@/constants';
+import { useSelectOptionState } from '@/contexts/SelectOptionProvider';
 
 interface Props {
   setGuideStep: Dispatch<React.SetStateAction<guideStepT>>;
@@ -11,6 +12,7 @@ interface Props {
 
 export default function GuideMainComplete({ setGuideStep }: Props) {
   const navigate = useNavigate();
+  const { dataList } = useSelectOptionState();
 
   const linkToComplete = () => {
     navigate(COMPLETE_URL);
@@ -22,13 +24,20 @@ export default function GuideMainComplete({ setGuideStep }: Props) {
 
   return (
     <S.MainContainer>
-      <img src={`${import.meta.env.VITE_STATIC_API_URL}/selective_option/4_1.png`} alt={`임시`} height={300} />
+      <img
+        src={`${import.meta.env.VITE_STATIC_API_URL}/${dataList[3].imgSrc}`}
+        alt={dataList[3].selectedName || '팰리세이드'}
+        height={300}
+      />
       <S.MainTitle>나만의 팰리세이드 견적 준비 완료!</S.MainTitle>
       <S.SelectedTrim>
         <S.Selected>선택된 트림</S.Selected>
         <S.Trim>팰리세이드 Le Blanc (르블랑)</S.Trim>
       </S.SelectedTrim>
-      <S.SubTitle>준비된 견적을 바로 확인하거나, {`\n`}옵션을 차례로 살펴보며 변경할 수 있어요.</S.SubTitle>
+      <S.SubTitle>
+        준비된 견적을 바로 확인하거나, <br />
+        옵션을 차례로 살펴보며 변경할 수 있어요.
+      </S.SubTitle>
       <S.ButtonLine>
         <RectButton type="recommended" page="ready" onClick={linkToComplete}>
           완성된 견적을 바로 볼게요
