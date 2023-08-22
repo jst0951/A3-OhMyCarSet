206ffcdae4b5f6191ed98db1f871e8f53e3b4726
Jaehong Kim <kjhonggg95@gmail.com>
2023-08-22 22:51:54 +0900

    refactor: @RequiredArgsConstructor 로 의존성 주입

--- backend/ohmycarset/src/main/java/com/softeer2nd/ohmycarset/controller/ExteriorColorController.java
+++ backend/ohmycarset/src/main/java/com/softeer2nd/ohmycarset/controller/ExteriorColorController.java
@@ -7,6 +7,7 @@ import com.softeer2nd.ohmycarset.service.ExteriorColorService;
 import com.softeer2nd.ohmycarset.service.TrimService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,13 +15,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @Tag(name = "외장 색상", description = "외장 색상 관련 API입니다.")
 public class ExteriorColorController {
 
     private final ExteriorColorService exteriorColorService;
-    public ExteriorColorController(TrimService trimService, ExteriorColorService exteriorColorService) {
-        this.exteriorColorService = exteriorColorService;
-    }
 
     @GetMapping(value = "/exterior_color")
     @Operation(summary = "[랜딩페이지]외장 색상", description = "모든 외장 색상 데이터를 제공합니다.")
