34c5462a8ba12381fe28edcbd38432b472badb05
Jaehong Kim <kjhonggg95@gmail.com>
2023-08-22 22:50:48 +0900

    refactor: @RequiredArgsConstructor 로 의존성 주입

--- backend/ohmycarset/src/main/java/com/softeer2nd/ohmycarset/controller/DefaultOptionController.java
+++ backend/ohmycarset/src/main/java/com/softeer2nd/ohmycarset/controller/DefaultOptionController.java
@@ -10,6 +10,7 @@ import com.softeer2nd.ohmycarset.service.DefaultOptionService;
 import com.softeer2nd.ohmycarset.service.TrimService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,13 +18,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @Tag(name = "기본 옵션", description = "기본 옵션 관련 API입니다.")
 public class DefaultOptionController {
     private final DefaultOptionService defaultOptionService;
-
-    public DefaultOptionController(DefaultOptionService defaultOptionService) {
-        this.defaultOptionService = defaultOptionService;
-    }
 
     @GetMapping(value = "/default_option")
     @Operation(summary = "[랜딩페이지]기본 옵션", description = "모든 기본 옵션 데이터를 제공합니다.")
