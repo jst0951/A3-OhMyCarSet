6faeb075153325be785198cbda31ed5b15ac74ec
yen001004 <yenn001004@gmail.com>
2023-08-24 00:00:30 +0900

    feat: 현대 로고 svg 변경

--- Frontend/src/asset/index.ts
+++ Frontend/src/asset/index.ts
@@ -1,4 +1,3 @@
-import hyundaiLogo from './logo/Hyundai_Logo.png';
 import mainBackgroundImage1 from '/images/main_palisade1.png';
 
-export { hyundaiLogo, mainBackgroundImage1 };
+export { mainBackgroundImage1 };
